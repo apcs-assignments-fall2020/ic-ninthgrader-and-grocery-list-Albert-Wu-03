c2fdae95cd69d18d4c3fef9c82fd903426e999dd
Albert-Wu-03 <22qwu@athenian.org>
2020-11-03 07:01:54 +0000

    List

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,27 @@
 public class GroceryList {
-    
+    private String[] arr;
+
+    public GroceryList(){
+        arr = new String[10];
+    }
+
+    public void add(String item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                arr[i] = item;
+                break;
+            }
+        }
+    }
+
+    public void remove(String item) {
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == item){
+                arr[i] = null;
+                if(arr[i] == null){
+                    arr[i+1] = arr[i];
+                }
+            }
+        }
+    }
 }
